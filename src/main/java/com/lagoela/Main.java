df20efb8a2c2da2a053bd787965a0b90c81124a5
Enzo Lagoela <lagoela.dev@gmail.com>
2025-01-23 19:26:04 -0300

    Adiciona métodos na classe dev, adiciona lógica para utilizacao das classes na classe Main

--- src/main/java/com/lagoela/Main.java
+++ src/main/java/com/lagoela/Main.java
@@ -1,38 +1,53 @@
 package com.lagoela;
 
-import com.lagoela.domain.Conteudo;
-import com.lagoela.domain.Curso;
-import com.lagoela.domain.Mentoria;
+import com.lagoela.domain.*;
 
 import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
 
-        Curso cursoJs = new Curso();
+        Curso poo = new Curso();
 
-        cursoJs.setTitulo("Curso de javascript");
-        cursoJs.setDescricao("Curso para paginas dinamicas usando JS");
-        cursoJs.setCargaHoraria(24);
+        poo.setTitulo("Curso de Programacao Orientada a Objetos");
+        poo.setDescricao("Curso para aprendizado de programacao orientada a objetos em Java");
+        poo.setCargaHoraria(1);
 
-        Curso cursoJava = new Curso();
+        Curso collections = new Curso();
 
-        cursoJava.setTitulo("Curso de Java");
-        cursoJava.setDescricao("Curso para criacao de back-end usando java");
-        cursoJava.setCargaHoraria(36);
+        collections.setTitulo("Curso de Collections");
+        collections.setDescricao("Curso para utilizacao de collections em Java");
+        collections.setCargaHoraria(1);
 
         Mentoria mentoriaJs = new Mentoria();
         mentoriaJs.setTitulo("Usando promises em javascript");
         mentoriaJs.setDescricao("Aprenda a usar promises e trabalhar assincronamente com Js");
         mentoriaJs.setDataDeInicio(LocalDate.now());
 
-        Conteudo conteudoJava = new Curso();
-        Conteudo conteudoJs = new Mentoria();
-
-        System.out.println(cursoJs);
-        System.out.println(cursoJava);
+        System.out.println(poo);
+        System.out.println(collections);
 
         System.out.println(mentoriaJs);
+
+        Bootcamp bootcamp1 = new Bootcamp();
+        bootcamp1.setNome("Bootcamp Java Developer");
+        bootcamp1.setDescription("Descricao teste Bootcamp Java Developer");
+        bootcamp1.getConteudos().add(poo);
+        bootcamp1.getConteudos().add(collections);
+        bootcamp1.getConteudos().add(mentoriaJs);
+
+        Dev devEnzo = new Dev();
+        devEnzo.setNome("Enzo");
+        devEnzo.inscreverBootcamp(bootcamp1);
+        System.out.println("Conteúdos Inscritos do Dev Enzo " + devEnzo.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos do Dev Enzo " + devEnzo.getConteudosConcluidos());
+
+        System.out.println("Dev " + devEnzo.getNome() + " progrediu.");
+        devEnzo.progredir();
+
+        System.out.println("Conteúdos Inscritos do Dev Enzo " + devEnzo.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos do Dev Enzo " + devEnzo.getConteudosConcluidos());
+        System.out.println("O Xp do dev " + devEnzo.getNome() + " é: " + devEnzo.calcularTotalXp());
 
     }
 }
